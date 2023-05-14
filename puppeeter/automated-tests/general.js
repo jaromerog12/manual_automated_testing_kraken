2631c0de0a6d2fa1ead04ee935c26f1208634662
andesleodiego24 <dl.bustamante@uniandes.edu.co>
2023-05-14 01:12:25 +0000

    Se agregan casos faltantes

--- puppeeter/automated-tests/general.js
+++ puppeeter/automated-tests/general.js
@@ -50,4 +50,52 @@ const Escenario2 = async () => {
     return;
 }
 
-module.exports = { Escenario1, Escenario2 };
+const Escenario14 = async () => {
+    const browser = await puppeteer.launch();
+    const escenario = "Escenario14";
+    const page = await browser.newPage();
+    const promises = [];
+    promises.push(page.waitForNavigation());
+    const login = new LoginPage(page, escenario);
+    const general = new GeneralPage(page, escenario)
+    await login.abrirPagina();
+    await login.ingresarCredenciales();
+    await login.tomarImagen("1.autenticacion");
+    await login.autenticar();
+    await general.abrirPagina();
+    await general.abrirDesplegableTwitter();
+    await general.tomarImagen("2.configuracion");
+    await general.modificarTwitter();
+    await general.tomarImagen("3.descripcion_modificada");
+    await general.guardar();
+    await general.abrirPagina();
+    await general.abrirDesplegableTwitter();
+    await general.tomarImagen("4.confirmacion");
+    return;
+}
+
+const Escenario15 = async () => {
+    const browser = await puppeteer.launch();
+    const escenario = "Escenario15";
+    const page = await browser.newPage();
+    const promises = [];
+    promises.push(page.waitForNavigation());
+    const login = new LoginPage(page, escenario);
+    const general = new GeneralPage(page, escenario)
+    await login.abrirPagina();
+    await login.ingresarCredenciales();
+    await login.tomarImagen("1.autenticacion");
+    await login.autenticar();
+    await general.abrirPagina();
+    await general.abrirDesplegableFacebook();
+    await general.tomarImagen("2.configuracion");
+    await general.modificarFacebook();
+    await general.tomarImagen("3.descripcion_modificada");
+    await general.guardar();
+    await general.abrirPagina();
+    await general.abrirDesplegableFacebook();
+    await general.tomarImagen("4.confirmacion");
+    return;
+}
+
+module.exports = { Escenario1, Escenario2, Escenario14, Escenario15 };
